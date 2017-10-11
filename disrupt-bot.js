63e17c92e418a5158d26fde6d2be432fd5e3be4d
smhdale <smhdale@live.com.au>
2017-10-11 18:04:20 +1000

    Started Wit.ai integration
    Bot should soon be able to handle intents related to the grad show.

--- disrupt-bot.js
+++ disrupt-bot.js
@@ -9,8 +9,38 @@ const paths = require('./paths')
 const disrupt = require('./disrupt')
 
 /**
+ * Dates
+ */
+
+const EXHIBITION_DATE = new Date(2017, 10, 22)
+
+function dateString (date) {
+  return [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate()
+  ].map(_ => _.toString()).join('-')
+}
+
+// Returns -1 for days before exhibition day, 0 for exhibition day, 1 for days after
+function exhibitionDayDiff () {
+  let now = new Date()
+  // Is it today?
+  if (dateString(now) === dateString(EXHIBITION_DATE)) {
+    return 0
+  } else {
+    let diff = now - EXHIBITION_DATE
+    return diff / Math.abs(diff)
+  }
+}
+
+/**
  * Message handling
  */
+
+function getNLPEntity (nlp, name) {
+  return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0]
+}
 
 function handleMessageReceived (event) {
   // Event data
@@ -24,20 +54,71 @@ function handleMessageReceived (event) {
   const messageText = message.text
   const messageAttachments = message.attachments
 
+  // Check for NLP entities
+  const nlpGreeting = getNLPEntity(message.nlp, 'wit/greetings')
+  const nlpIntent = getNLPEntity(message.nlp, 'intent')
+
   // Get sender's profile details
   getUser(senderID).then(user => {
-    // Check if the user asked to be disrupted
-    if (messageText.toLowerCase().indexOf('disrupt me') > -1) {
-      api.sendTextMessage(senderID, 'Working on that...').catch(console.error)
+    switch (nlpIntent) {
+      case 'disrupt_user':
+        // Disrupt the user's profile picture and send them an animated .gif
+        return doProfileDisrupt(senderID)
 
-      // Create disrupted image
-      disrupt.disruptImage(senderID).then(filename => {
-        return api.sendPictureMessage(senderID, paths.serve(filename))
-      }).catch(console.error)
-    } else {
-      api.sendTextMessage(senderID, `I'm sorry, ${user.first_name}, I'm afraid I can't do that.`).catch(console.error)
+      case 'exhibition_time':
+        // Send info about the date and time of the exhibition
+        return sendExhibitionDate(senderID)
+
+      case 'exhibition_countdown':
+        // Send number of days until exhibition starts,
+        // handling day-of and days after
+        return api.sendTextMessage(senderID, `I'm sorry, ${user.first_name}, I'm afraid I can't do that.`)
+
+      default:
+        return api.sendTextMessage(senderID, `I'm sorry, ${user.first_name}, I'm afraid I can't do that.`)
     }
   }).catch(console.error)
+}
+
+/**
+ * Informs the user about what date the exhibition is happening on
+ */
+function sendExhibitionDate (fbid) {
+  let date = ' on the 22nd of November, 2017.'
+  switch (exhibitionDayDiff()) {
+    case -1:
+      date = 'will be taking place' + date
+      break
+    case 1:
+      date = 'took place' + date
+      break
+    default:
+      date = 'is happening today!'
+      break
+  }
+  return api.sendTextMessage(senderID, `The QUT DISRUPT grad show ${date}`)
+}
+
+/**
+ * Gives a countdown of days until the grad show
+ */
+function sendExhibitionCountdown (fbid) {
+
+}
+
+/**
+ * Creates a disrupted, animated GIF version of the user's profile picture
+ */
+function doProfileDisrupt (fbid) {
+  let progressMessage = api.sendTextMessage(senderID, 'Working on that...')
+  let disruptedProfile = disrupt.disruptImage(senderID).then(filename => {
+    return api.sendPictureMessage(senderID, paths.serve(filename))
+  })
+
+  return Promise.all([
+    progressMessage,
+    disruptedProfile
+  ])
 }
 
 /**
